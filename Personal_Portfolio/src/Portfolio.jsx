6c500f75c79ce0bf8821c5eb30023fb2f891aa2d
Ryanyan89 <ryanyan1404@hotmail.com>
2024-03-17 17:34:08 -0700

    Issue #1. Created the vite framework to start coding the website

---
+++ Personal_Portfolio/src/Portfolio.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>test</p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
